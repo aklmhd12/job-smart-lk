7cbfbabc8b6e765b548a0040c3eb8a0f1b654f2e
aklmhd12 <aklmhd12@gmail.com>
2026-09-20 09:02:07 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const { Pool } = require('pg');
+const cors = require('cors');
 
 const app = express();
+
 app.use(express.json());
+app.use(cors());
 
 const PORT = Number(process.env.PORT) || 5000;
 
